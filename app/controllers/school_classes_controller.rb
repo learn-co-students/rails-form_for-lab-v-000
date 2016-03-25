69d8d647f502565cab2fe53fea35abedb8d36859
Brian Holland <brian.holland@proatria.com>
2016-03-24 23:17:11 -0600

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,25 @@
+class SchoolClassesController < ApplicationController
+  def index
+    @classes = SchoolClass.all
+  end
+
+  def new
+  end
+
+  def create
+    @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+  end
+
+end
