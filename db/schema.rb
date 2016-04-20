b4fa1fed2f48fbd508fccfcffd19558a7bca747f
e-baker <eric@ericbaker.me>
2016-04-19 23:35:08 -0400

    update database to have correct structure

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 2) do
 
-  create_table "school_class", force: :cascade do |t|
+  create_table "school_classes", force: :cascade do |t|
     t.string  "title"
     t.integer "room_number"
   end
