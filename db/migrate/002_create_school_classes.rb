82a535e49a62362caa9786181845e0cf2ece2d20
adriandave13 <adrian@paradoxlogic.net>
2016-05-11 14:40:56 -0400

    Done.

---
+++ db/migrate/002_create_school_classes.rb
@@ -0,0 +1,13 @@
+class CreateSchoolClasses < ActiveRecord::Migration
+  def up
+    create_table :school_classes do |t|
+      t.string :title
+      t.integer :room_number
+    end
+  end
+
+  def down
+    drop_table :school_classes
+  end
+
+end
