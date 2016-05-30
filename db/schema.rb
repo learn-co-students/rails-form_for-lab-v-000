0e91df79bb8c812d9df5d65990a9a2565aa74cbb
PeteAJ <peter.jacobson@gmail.com>
2016-05-30 14:34:43 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20160529224752) do
+
+  create_table "school_classes", force: :cascade do |t|
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
