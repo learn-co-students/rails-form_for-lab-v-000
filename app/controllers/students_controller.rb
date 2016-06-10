c2dd178d764c2782b6c7256a5146a367be6e57cd
reifnotreef <bridgerrhammond@gmail.com>
2016-06-10 22:14:42 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find_by(id: params[:id])
+    @student_name = @student.to_s
+  end
+
+  def edit
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+
+end
