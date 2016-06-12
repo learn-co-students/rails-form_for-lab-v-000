24d237bc64726e52dbd82464f781eb71775f3713
smithWEBtek <brad@smithwebtek.com>
2016-06-12 13:05:35 -0400

    Done.

---
+++ db/migrate/20160526220339_add_title_room_number_to_school_classes.rb
@@ -0,0 +1,6 @@
+class AddTitleRoomNumberToSchoolClasses < ActiveRecord::Migration
+  def change
+    add_column :school_classes, :title, :string
+    add_column :school_classes, :room_number, :integer
+  end
+end
