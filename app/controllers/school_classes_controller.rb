d655da40b72b5c4075fc0ee8ea0254fe1f3dee37
Joseph Lim <joseph.d.lim@gmail.com>
2016-08-03 19:08:40 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,19 +6,23 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
+    @school_class.update(params.require(:school_class))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
   def show
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,19 +6,24 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    @student.update(params.require(:student))
     @student.save
     redirect_to student_path(@student)
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
