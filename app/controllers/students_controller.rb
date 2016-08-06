65a7a471357ce8cc22fd1e152d583443455f0ac8
jwolfe890 <jwolfe890@gmail.com>
2016-08-06 16:58:17 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class StudentsController < ApplicationController
+
+    def show
+        @student = Student.find(params[:id])
+    end 
+
+    def new
+       @student = Student.create
+    end
+
+    def edit
+       @student = Student.find(params[:id])
+    end
+
+    def create
+       @student = Student.new(student_params(:first_name, :last_name))
+       @student.save
+       redirect_to student_path(@student)
+    end 
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end 
+
+    private 
+
+    def student_params(*args)
+        params.require(:student).permit(*args)
+    end
+
+
+end
