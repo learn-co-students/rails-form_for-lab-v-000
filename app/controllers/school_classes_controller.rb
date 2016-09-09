b47910cddbc7c24f4f0d7235390b37519a45e259
Yechiel Kalmenson <chilikkal@gmail.com>
2016-09-09 00:57:40 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,32 @@
 class SchoolClassesController < ApplicationController
 
+	def show
+		@school_classes = SchoolClasses.find(params[:id])
+	end
+
+	def new
+		@school_classes = SchoolClasses.new
+	end
+
+	def create
+	  @school_classes = SchoolClasses.create(school_classes_params)
+	  @school_classes.save
+	  redirect_to school_classes_path(@school_classes)
+	end
+
+	def update
+	  @school_classes = SchoolClasses.find(params[:id])
+	  @school_classes.update(school_classes_params(:title))
+	  redirect_to school_classes_path(@school_classes)
+	end
+
+	def edit
+	  @school_classes = SchoolClasses.find(params[:id])
+	end
+
+	private
+
+	def school_classes_params(*args)
+		params.require(:school_classes).permit(*args)
+	end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,31 @@
 class StudentsController < ApplicationController
+  def show
+    @students = Student.find(params[:id])
+  end
 
+  def new
+    @students = Student.new
+  end
+
+  def create
+    @students = Student.create(students_params)
+    @students.save
+    redirect_to students_path(@students)
+  end
+
+  def update
+    @students = Student.find(params[:id])
+    @students.update(students_params(:title))
+    redirect_to students_path(@students)
+  end
+
+  def edit
+    @students = Student.find(params[:id])
+  end
+
+  private
+
+  def students_params(*args)
+    params.require(:students).permit(*args)
+  end
 end
