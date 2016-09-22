a7b85ea0553cd8f103dabdca62c5551074e94e55
William Jones <enquiries@forestfed.com>
2016-09-22 03:46:58 +0000

    use form_for

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,32 +1,31 @@
-class School_Classes_Controller < ApplicationController
-
+class SchoolClassesController < ApplicationController
+#  q
 	def index
-		@school_classes = School_class.all
+		@school_classes = SchoolClass.all
 	end
 
 	def show
-		@school_class = School_class.find(params[:id])
+		@school_class = SchoolClass.find(params[:id])
 	end
 
 	def new
-		@school_class = School_class.new
+		@school_class = SchoolClass.new
 	end
 
 	def create
-	  @school_class = School_class.new
-	  @school_class.title = params[:title]
-	  @school_class.description = params[:description]
-	  @school_class.save
-	  redirect_to post_path(@school_class)
-	end
+    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
 
 	def edit
-		@school_class = School_class.find(params[:id])
+		@school_class = SchoolClass.find(params[:id])
 	end
 
 	def update
-	  @school_class = School_class.find(params[:id])
+	  @school_class = SchoolClass.find(params[:id])
 		@school_class.update(params.require(:school_class))
 	  redirect_to school_class_path(@school_class)
 	end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,7 +24,7 @@ class StudentsController < ApplicationController
 
 	def update
 	  @student = Student.find(params[:id])
-		@student.update(student_params(:first_name, :last_name)
+		@student.update(student_params(:first_name, :last_name))
 	  redirect_to student_path(@student)
 	end
 
