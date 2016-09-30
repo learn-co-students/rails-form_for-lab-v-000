d2463d9e027e7c26bc78a75b6f4d803c4cdac4ae
Kathleen <github email address>
2016-09-30 11:37:36 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+
+	def new
+		@student = Student.new
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def create
+		@student = Student.create(params[:student])
+		redirect_to student_path(@student)
+	end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def update
+		@student = Student.find(params[:id])
+		@student.update(params[:student])
+		redirect_to student_path(@student)
+	end
+
+
+end
