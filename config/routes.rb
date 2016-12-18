8f21b22b5b23e9b872140f13d70c6a9a83a4b05c
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-18 02:09:06 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources 'schoolclasses', only: [:show, :update, :edit, :new, :create]
+  resources 'school', only: [:show, :update, :edit, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
