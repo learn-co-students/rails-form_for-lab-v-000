903121e6efbd56bdbb44fdf592c62146290044dd
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-01-09 05:35:30 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,37 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(params_strong(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params_strong(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def params_strong(*args)
+    params.require(:student).permit(*args)
+  end
+
+end
