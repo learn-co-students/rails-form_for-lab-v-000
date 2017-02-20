015cee38096cb3311bf6760b65666698daba0419
warrend <warrend555@gmail.com>
2017-02-20 13:26:57 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,35 @@
+class SchoolClassesController < ApplicationController
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
+	def create
+		@school_class = SchoolClass.new(sc_params(:title, :room_number))
+		@school_class.save
+		redirect_to school_class_path(@school_class)
+	end
+
+	def show
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def edit
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def update
+		@school_class = SchoolClass.find(params[:id])
+		@school_class.update(sc_params(:title, :room_number))
+		@school_class.save
+
+		redirect_to school_class_path(@school_class)
+	end
+
+	private
+	 
+	def sc_params(*args)
+	  params.require(:school_class).permit(*args)
+	end
+
+end
