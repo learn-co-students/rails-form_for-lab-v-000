a62a6309275ce8e1cd7f9461146ae91f0a7d5c07
Nick Bair <bairinc@gmail.com>
2017-06-05 16:23:22 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :students, except: [:destory, :index]
+  resources :school_classes, except: [:destory, :index]
+
   # Example resource route with options:
   #   resources :products do
   #     member do
