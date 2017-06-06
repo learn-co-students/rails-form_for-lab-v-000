5016732e637f35f2a526afd86282ca245a217e04
David Mars <david.w.mars@my.occc.edu>
2017-06-06 00:44:07 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,29 @@
+#~~~HAVICK WAS HERE~~~
+
+class StudentsController < ApplicationController
+
+  def new
+    @student=Student.new
+  end
+
+  def create
+    @student = Student.create(params.require(:student))
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student=Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+#~~~HAVICK WAS HERE~~~
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+resources :students, only: [:new, :create, :show, :edit, :update]
+resources :school_classes, only: [:new, :create, :show, :edit, :update]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
