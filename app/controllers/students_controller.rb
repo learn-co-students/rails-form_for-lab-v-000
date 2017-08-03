fc57c706f569d6915a5cb60062e48034d61b3b09
Richard-Frost <frost.rchrd@gmail.com>
2017-08-02 21:04:59 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+  require 'pry'
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def edit
+    @edit_student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :school_classes, only: [:show, :new, :create, :edit]
+  resources :students, only: [:create, :new, :show, :edit]
+  patch 'school_classes/:id', to: 'school_classes#update'
+  patch 'students/:id', to: 'students#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
