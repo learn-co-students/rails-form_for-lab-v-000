9371d3a3b54d8f6652ba7815ca5ba40947e61282
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-09 15:15:45 -0700

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,17 +4,27 @@ class SchoolClassesController < ApplicationController
     end
 
     def show
-        @school_class = SchoolClasses.find(params[:id])
+        @school_class = SchoolClass.find(params[:id])
     end
 
     def new
+        @school_class = SchoolClass.new
     end
 
     def create
-
+        @school_class = SchoolClass.create(params.require(:school_class))
+        
+        redirect_to school_class_path(@school_class)
     end
 
     def edit
+        @school_class = SchoolClass.find(params[:id])
     end
 
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(params.require(:school_class))
+
+        redirect_to school_class_path(@school_class)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
-  resources :schoolclasses, only: [:index, :show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
 
 end
