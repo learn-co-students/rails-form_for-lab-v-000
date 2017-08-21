7f27e4ac0fed28071dba0d6482af5a3af323c282
gb23 <gregbenj@hotmail.com>
2017-08-21 13:58:10 -0700

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,8 +8,7 @@ class SchoolClassesController < ApplicationController
     end
     
     def create
-        @school_class = SchoolClass.new(params.require(:school_class))
-            # school_class_params(:title, :room_number))
+        @school_class = SchoolClass.new(school_class_params(:title, :room_number))
         @school_class.save
 
         redirect_to school_class_path(@school_class)
