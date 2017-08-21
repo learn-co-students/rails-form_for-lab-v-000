30d7253206a5fd4288dfeb88fddac1a1e98f7c07
gb23 <gregbenj@hotmail.com>
2017-08-21 13:22:20 -0700

    update student create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  resources :school_class, only: [:index, :show, :new, :create, :edit, :update]
+
 
   
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@
 ActiveRecord::Schema.define(version: 2) do
 
   create_table "school_classes", force: :cascade do |t|
-    t.string  "title"
-    t.integer "room_number"
+    t.string   "title"
+    t.integer  "room_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "students", force: :cascade do |t|
