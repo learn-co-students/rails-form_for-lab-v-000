b35b1f22786404793010c50fc6680fa0407dc592
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-01 20:53:26 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,6 +3,26 @@ class SchoolClassesController < ApplicationController
   def index
   end
 
-  
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :students, :school_classes, only: [:index, :new, :create]
 
   get '/student/:id', to: 'students#show', as: 'student'
+  get '/school_class/:id', to: 'school_classes#show', as: 'school_class'
 # this created the route to students#index
 
 
