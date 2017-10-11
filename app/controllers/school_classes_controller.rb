fab2e5d2f1b967966c921fff919064847c87803d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-11 18:54:13 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,34 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(params.require(:school_class))
+  	@school_class.save
+  	redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+  	redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def post_params(*args)
+  	params.require(:school_class).permit(*args)
+	end
+
+
+end
