0f50674e7d7127cc044c78c6d0df35eace531c6b
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-15 21:22:43 -0700

    Done

--- db/migrate/002_create_school_class.rb
+++ db/migrate/002_create_school_class.rb
@@ -1,6 +1,6 @@
 class CreateSchoolClass < ActiveRecord::Migration
   def change
-    create_table :school_class do |t|
+    create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
 
