674b06c8c5631e6d98f652f4ce4aad47888ce567
aisayo <aisayo@icloud.com>
2017-10-24 18:25:33 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,8 +6,8 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
+    @school_class.title = params[:school_class][:title]
+    @school_class.room_number = params[:school_class][:room_number]
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
