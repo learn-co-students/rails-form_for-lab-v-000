de125b64727babaa3c04ca955d7dc8b6ad18319d
lpassamano <leighayna.josele@gmail.com>
2017-10-29 12:34:32 -0400

    added instance variables to #index and #show

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,10 +1,10 @@
 class SchoolClassesController < ApplicationController
   def index
-
+    @school_classes = SchoolClass.all
   end
 
   def show
-
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def new
