4326c4423152ff0cf2e0c848d398b53d1370aeae
lpassamano <leighayna.josele@gmail.com>
2017-10-29 12:47:29 -0400

    added SchoolClass create controller action

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,11 +8,13 @@ class SchoolClassesController < ApplicationController
   end
 
   def new
-    @school_class = SchoolClass.new 
+    @school_class = SchoolClass.new
   end
 
   def create
-
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
