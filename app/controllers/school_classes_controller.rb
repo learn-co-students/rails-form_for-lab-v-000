294552921c404e3fcd53fba11faa7473759778d5
lpassamano <leighayna.josele@gmail.com>
2017-10-29 13:08:05 -0400

    added SchoolClass #edit and #update controller methods

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -18,10 +18,12 @@ class SchoolClassesController < ApplicationController
   end
 
   def edit
-
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
-
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class).permit(:title, :room_number))
+    redirect_to school_class_path(@school_class)
   end
 end
