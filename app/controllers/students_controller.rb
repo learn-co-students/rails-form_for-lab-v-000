59ceb1d590583418afad6fb62f3cb7861f98d1e3
lpassamano <leighayna.josele@gmail.com>
2017-10-29 13:02:04 -0400

    added Student controller actions #new and #create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,11 +8,13 @@ class StudentsController < ApplicationController
   end
 
   def new
-
+    @student = Student.new
   end
 
   def create
-
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
