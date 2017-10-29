da642c9ba37e2732f2c117a7606eb39a31d90d9f
lpassamano <leighayna.josele@gmail.com>
2017-10-29 12:52:11 -0400

    stubbed Student controller actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,25 @@
 class StudentsController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update 
+
+  end
 end
