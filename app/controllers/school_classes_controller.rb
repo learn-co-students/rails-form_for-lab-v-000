f9c2eb59017d7454421f7543f8e69b418f8afc07
lpassamano <leighayna.josele@gmail.com>
2017-10-29 12:20:49 -0400

    stubbed SchoolClassesController methods

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,25 @@
 class SchoolClassesController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
