5ca99745647f155fcde93a136e673ce24067d77d
Cole Martin <mar08049@gmail.com>
2017-10-31 18:56:02 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,28 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
+  end
+end
