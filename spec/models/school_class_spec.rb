f779d8ea3100eb4d31c820e1dfb496fde7364b86
github username <github email address>
2017-11-06 17:47:44 -0500

    Done.

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe SchoolClass, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SchoolClass do
+  before(:each) do
+    @school_class = SchoolClass.create!(title: "Coding", room_number: 424)
+  end
+
+  it 'can be created' do
+    expect(@school_class).to be_valid
+  end
+
 end
