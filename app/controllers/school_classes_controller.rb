6097b7fe7ebd29a50f53deb1c307eb531fcae95a
github username <github email address>
2017-11-18 22:21:15 -0500

    working on /school_classes/new

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,18 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
