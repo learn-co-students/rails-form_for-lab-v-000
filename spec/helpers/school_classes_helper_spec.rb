bd2504d1ef9f1608fa4fb88b65cdd2838f97d709
github username <github email address>
2017-12-04 14:44:18 -0500

    Done.

---
+++ spec/helpers/school_classes_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SchoolClassesHelper. For example:
+#
+# describe SchoolClassesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe SchoolClassesHelper, :type => :helper do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe SchoolClassesHelper do
+    describe "to_s" do
+      it "concats two strings with spaces" do
+        expect(helper.to_s("this","that")).to eq("this that")
+      end
+    end
+  end
+end
