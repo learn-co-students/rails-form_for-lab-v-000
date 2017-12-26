da5658a7d65df305f3ebc424d0fa309ae202ab13
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-26 16:08:52 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,6 +8,7 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.create(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
   end
 
   def show
