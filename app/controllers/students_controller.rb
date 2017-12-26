249d0fe9509d082cb0a99704435787f357ef6cc1
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-26 16:20:50 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create()
+    @student = Student.create(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
@@ -23,7 +23,7 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find(params[:id])
-    @student.update(student_params[:first_name, :last_name])
+    @student.update(params[:student])
     redirect_to student_path(@student)
   end
 
