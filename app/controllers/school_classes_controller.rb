643d23de6072b8141161d1912e1800ae704161dd
Olena Ageyeva <eageeva@hotmail.com>
2018-01-03 00:13:07 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,6 +14,14 @@ class SchoolClassesController<ApplicationController
   end
 
   def edit
+    @class = SchoolClass.find(params[:id])
+
+  end
+
+  def update
+    @class = SchoolClass.find(params[:id])
+    @class.update(class_params(:title, :room_number))
+   redirect_to school_class_path(@class)
   end
 
   def create
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,14 +9,28 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student=Student.new
   end
 
   def create
-    @student= Student.new
-    @student.first_name=params[:first_name]
-    @student.last_name=params[:last_name]
+    @student= Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
+  end
+
+  def edit
+    @student=Student.find(params[:id])
+  end
+
+  def update
+    @student=Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 
 end
