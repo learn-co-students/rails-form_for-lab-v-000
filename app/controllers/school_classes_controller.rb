69ae0b4497119ef811666629885c3719f3ad7f56
EC2 Default User <ec2-user@ip-172-31-46-51.us-west-2.compute.internal>
2018-01-05 05:25:28 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,40 @@
+class SchoolClassesController < ApplicationController
+	def index
+		@school_classs = SchoolClass.all
+	end
+
+	def show
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
+	def create
+	  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+	  @school_class.save
+	  redirect_to school_class_path(@school_class)
+	end
+	 
+	def update
+	  @school_class = SchoolClass.find(params[:id])
+	  @school_class.update(school_class_params(:title))
+	  redirect_to school_class_path(@school_class)
+	end
+
+	def edit
+	  @school_class = SchoolClass.find(params[:id])
+	end
+	
+	private
+ 
+ 
+	# We pass the permitted fields in as *args;
+	# this keeps `school_class_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action
+	def school_class_params(*args)
+	  params.require(:school_class).permit(*args)
+	end
+end
