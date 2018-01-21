6f0b05454b4e8a84f5e73d162a4a2da469b08190
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-21 21:33:32 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+    @student = Student.new(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
