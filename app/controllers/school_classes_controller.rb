c7289b706936e57ec66bd6963a19b125e5852adb
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-21 21:09:57 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,16 @@
 class SchoolClassesController < ApplicationController
 
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
 end
