20b45bfb0aa720f9880314b51d11be6b0f8a093a
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-21 21:21:17 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -17,4 +17,10 @@ class SchoolClassesController < ApplicationController
   def edit
     @school_class = SchoolClass.find(params[:id])
   end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
+  end
 end
