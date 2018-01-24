b60013a632e7a380c26a27421fabdfa0676b1c1b
Jacqueline Lee <anginblu@gmail.com>
2018-01-24 06:43:59 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,16 +8,28 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+		@student = Student.new
+	end
+  
   def create
-
+    @student = Student.create(post_params(:first_name, :last_name))
   end
 
   def edit
-
+    @student = Student.find(params[:id])
   end
 
   def update
-
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+	  redirect_to student_path(@student)
   end
 
+  private 
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :students, only: [:index, :new, :create, :edit, :update]
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
 
-  resources :school_classes, only: [:index, :new, :create, :edit, :update]
-  post 'school_classes/create' => 'school_classes#show'
-  post 'school_classes/:id/edit' => 'school_classes#show'
+  patch 'schools/:id', to: 'schools#update'
+  patch 'school_classes/:id', to: 'school_classes#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
