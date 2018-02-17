953e6b0fca29594e42194e04e16cad6f3780eba5
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-02-17 15:27:28 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+class SchoolClassesController < ActionController::Base
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def edit
+    school_class
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+	  @school_class.save
+	  redirect_to school_class_path(@school_class)
+  end
+
+  def update
+    school_class
+    @school_class.update(school_class_params(:title))
+	  redirect_to school_class_path(@school_class)
+	end
+
+  def show
+    school_class
+  end
+
+  private
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
+  def school_class
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+end
