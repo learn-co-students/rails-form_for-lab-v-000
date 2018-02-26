3979810e9ede6ffd66c979e624e7420b6c20b8be
Caroline Shields <cbshields@outlook.com>
2018-02-26 23:34:59 +0000

    student controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit]
-  resources :schoolclass, only: [:index, :show, :new, :create, :edit]
+  resources :school_classes, only: [:index, :show, :new, :create, :edit]
   patch 'students/:id', to: 'students#update'
   patch 'schoolclass/:id', to: 'schoolclass#update'
 
