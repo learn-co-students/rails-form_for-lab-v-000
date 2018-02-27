160a605170aebb008eb5ff4f43a41cb0ddecc630
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-02-27 04:59:06 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ Rails.application.routes.draw do
   #   end
 
   resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
-
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
 end
