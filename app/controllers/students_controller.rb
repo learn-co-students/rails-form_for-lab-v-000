a54caf695ab171d771935b2626a866911c186ea3
Caroline Shields <cbshields@outlook.com>
2018-02-27 00:00:01 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,12 @@
 class StudentsController < ApplicationController
 
-  def index
-    @students = Student.all
-  end
 
   def show
     @student = Student.find(params[:id])
   end
 
   def new
-
+    @student = Student.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit]
   resources :school_classes, only: [:index, :show, :new, :create, :edit]
   patch 'students/:id', to: 'students#update'
-  patch 'schoolclass/:id', to: 'schoolclass#update'
+  patch 'school_classes/:id', to: 'school_classes#update'
 
 
 
