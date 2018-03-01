6c04f0e0e7fc3fdb59252ffefd3a33a08f9e9102
rh24 <ms.rebeccahong@gmail.com>
2018-03-01 16:43:06 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+class SchoolClassesController < ApplicationController
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def index
+  end
+
+  def create
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    set_school_class
+  end
+
+  def edit
+    set_school_class
+  end
+
+  def update
+    set_school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def set_school_class
+    @school_class ||= SchoolClass.find(params[:id])
+  end
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+end
