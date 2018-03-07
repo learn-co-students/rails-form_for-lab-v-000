2412ae5104fe88f99cfb44e0c358e31fcdd8ba94
funemployeddad <funemployeddad@gmail.com>
2018-03-07 12:14:49 -0700

    copy/pasted all from school_classes to students, and updated with correct params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,40 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def show
+    set_student
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
+    set_student
   end
 
   def update
+    set_student
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
