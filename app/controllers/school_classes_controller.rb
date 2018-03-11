05aa35eae445446ed1db9c45bb638436b96592b2
jacobsilver2 <jacobsilver2@mac.com>
2018-03-10 19:19:08 -0500

    making progress

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,26 @@
 class SchoolClassesController < ApplicationController
+  def index
+    @class = SchoolClass.all
+  end
+
+  def show
+    @class = SchoolClass.find(params[:id])
+  end
+  
+  def new
+    @class = SchoolClass.new
+  end
+  
+  def create
+		@class = SchoolClass.new(title: params[:title], room_number: params[:room_number])
+	  @class.save
+	  redirect_to school_class_path(@class)
+  end
+
+
+  
+  # private
+  #   def post_params(*args)
+  #     params.require(:post).permit(:title, :room_number)
+  #   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310223307) do
+ActiveRecord::Schema.define(version: 20180310225415) do
 
   create_table "school_classes", force: :cascade do |t|
     t.string  "title"
