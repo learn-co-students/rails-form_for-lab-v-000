9e2b937fe94919a8e6b6b47300b3084e431fdeab
jacobsilver2 <jacobsilver2@mac.com>
2018-03-10 19:21:02 -0500

    created students routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
 
   
 
