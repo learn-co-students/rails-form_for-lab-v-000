cc8b572a1d73d6737d8c03164f0a1bc795c8fbe7
jacobsilver2 <jacobsilver2@mac.com>
2018-03-10 19:24:08 -0500

    created studentscontroller stuff

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new()
+    @student.save
+    redirect_to student_path(@student)
+  end
+  
 end
+
+
+
+
