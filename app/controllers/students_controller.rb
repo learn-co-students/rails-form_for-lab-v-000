6263f6e2c1a28edd11307fec4d1d0e2d220e8be5
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 19:35:14 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,16 @@
 class StudentsController < ApplicationController
-  def new
+  before_action :set_student, only: [:show, :edit]
 
+  def new
+    @student = Student.new
   end
 
   def create
-
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   def show
-
   end
 
   def edit
@@ -16,6 +18,16 @@ class StudentsController < ApplicationController
   end
 
   def update
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
 
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
