a7f5a7e2304bec7090fc4dc48977f68c673d04e7
EC2 Default User <ec2-user@ip-172-31-15-206.us-east-2.compute.internal>
2018-03-18 20:07:04 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,7 +13,8 @@ class SchoolClassesController < ApplicationController
     end
     
     def create
-        @school_class = SchoolClass.create(params[:school_class])
+        @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+        @school_class.save
 	    redirect_to school_class_path(@school_class)
     end
     
@@ -23,8 +24,14 @@ class SchoolClassesController < ApplicationController
     
     def update
         @school_class = SchoolClass.find(params[:id])
-        @school_class.update(params.require(:school_class))
+        @school_class.update(school_class_params(:title, :room_number))
         redirect_to school_class_path(@school_class)
+    end
+    
+    private
+    
+    def school_class_params(*args)
+        params.require(:school_class).permit(*args)    
     end
     
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,8 @@ class StudentsController < ApplicationController
     end
     
     def create
-   	    @student = Student.create(params[:student])
+   	    @student = Student.new(student_params(:first_name, :last_name))
+   	    @student.save
 	    redirect_to student_path(@student)
     end
     
@@ -23,8 +24,14 @@ class StudentsController < ApplicationController
     
     def update
         @student = Student.find(params[:id])
-        @student.update(params.require(:student))
+        @student.update(student_params(:first_name, :last_name))
         redirect_to student_path(@student)
+    end
+    
+    private
+    
+    def student_params(*args)
+        params.require(:student).permit(*args)    
     end
     
 end
