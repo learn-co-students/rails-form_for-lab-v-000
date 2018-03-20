b4615d21002c98301eca813fafaf41bc1d23baca
remclay <remmy.lauren@gmail.com>
2018-03-20 18:22:40 +1000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find_by(params[:id])
+  end
+
+  def edit
+    @student = Student.find_by(params[:id])
+  end
+
+  def update
+    @student = Student.find_by(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+end
