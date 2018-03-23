505c278ae2e6ee6dc57b7b44cc25b72774b5513c
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-23 15:53:10 -0400

    Create new/edit forms for students & SchoolClasses using form_for

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(post_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+
+
+private
+
+  def post_params(*args)
+    params.require(:student).permit(*args)
+  end
+
+end
