bf162627fd7f747b532f4b8234bb90512e9d5d2f
jendobes <jdobak1@gmail.com>
2018-04-01 18:48:13 +0200

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(post_params)
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params)
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+private
+
+  def post_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+end
