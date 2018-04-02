4825783a07add5613a9242ab77effe2608254b12
Naomi Dennis <njdennis94@gmail.com>
2018-04-02 04:26:56 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :school_classes
+  resources :students
+
+  root 'school_classes#index'
 end
