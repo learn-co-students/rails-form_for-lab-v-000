b49732f4c0f4e75627d2cfb2de041dfd43a9a761
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-04 23:34:58 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,6 @@
 class SchoolClassesController < ApplicationController
   def new
+    #do we need anything here?
   end
   def edit
     @school_class= SchoolClass.find(params[:id])
@@ -9,9 +10,11 @@ class SchoolClassesController < ApplicationController
   end
   def create
     @school_class= SchoolClass.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to school_class_path(@school_class)
   end
   def update
     @school_class= SchoolClass.find(params[:id])
-    @school_class.update()
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,10 +9,12 @@ class StudentsController < ApplicationController
   end
   def create
     @student= Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
   def update
     @student=Student.find(params[:id])
-    @student.update(fist_name: params[:first_name], last_name: params[:last_name])
-    redirect_to
+  #  @student.update(fist_name: params[:first_name], last_name: params[:last_name])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
   end
 end
