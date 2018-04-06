f9bf6eed306dc71309acd4111e5927d84b213682
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-06 19:51:05 +0000

    Done.

---
+++ db/migrate/20180406191917_create_schoolclass.rb
@@ -0,0 +1,8 @@
+class CreateSchoolclass < ActiveRecord::Migration
+  def change
+    create_table :schoolclasses do |t|
+      t.string :title
+      t.integer :room_number 
+    end
+  end
+end
---
+++ db/migrate/20180406194111_rename_school_class_table.rb
@@ -0,0 +1,5 @@
+class RenameSchoolClassTable < ActiveRecord::Migration
+  def change
+    rename_table :schoolclasses, :school_classes
+  end
+end
