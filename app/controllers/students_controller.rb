d3e764e961a4af500d02ddd90cd6c1ef5d1203f5
Susan Fowler <infopro94@gmail.com>
2018-04-10 15:27:06 -0500

    finished coding controllers and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,4 +18,10 @@ class StudentsController < ApplicationController
 		@student = Student.find(params[:id])
 	end
 
+	def update
+		@student = Student.find(params[:id])
+		@student.update(params.require(:student))
+		redirect_to student_path(@student)
+	end
+
 end
