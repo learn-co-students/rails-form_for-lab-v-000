5d1294f10731231f27c462151f0ca8af22687355
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-13 18:29:14 +0100

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe Student do
   before(:each) do
-    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+    @student = Student.create!(first_name: 'Daenerys', last_name: 'Targaryen')
   end
 
   it 'can be created' do
     expect(@student).to be_valid
   end
-
 end
