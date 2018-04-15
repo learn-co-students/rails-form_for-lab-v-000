7b6852ed4314ef1bef229c32168f14f77cbae94e
github username <github email address>
2018-04-15 12:02:45 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,20 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+    @school_classes = SchoolClasses.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+end
