cc86515069a13ec056a2cb04956aef48cfcf5989
github username <github email address>
2018-04-15 16:02:17 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,20 +1,34 @@
 class SchoolClassesController < ApplicationController
 
+  def show
+    @school_class = set_school_class
+  end
+
   def new
-    @school_classes = SchoolClasses.new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
-  def show
-  end
+
 
   def edit
+      @school_class = set_school_class
   end
 
   def update
+      @school_class = set_school_class
+      @school_class.update(params.require(:school_class))
+      redirect_to school_class_path(@school_class)
   end
 
+  private
+    def set_school_class
+      @school_class = SchoolClass.find_by(params[:id])
+    end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,20 +1,37 @@
 class StudentsController < ApplicationController
 
+  def show
+    @student = set_student
+  end
+
+
   def new
     @student = Student.new
   end
 
   def create
+    @student = Student.new(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
   end
 
-  def show
-  end
 
   def edit
+    @student = set_student
   end
 
   def update
+    @student = set_student
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
   end
+
+
+  private
+    def set_student
+      @student = Student.find_by(params[:id])
+    end
+
 
 
 
