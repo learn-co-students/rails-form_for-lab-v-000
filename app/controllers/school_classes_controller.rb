1dd7719326360e09b53245761eddee5fc2d0f915
github username <github email address>
2018-04-19 12:29:49 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,30 +1,25 @@
 class SchoolClassesController < ActionController::Base
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
   def new
     @school_class = SchoolClass.new
   end
 
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
   def create
-    @school_class = SchoolClass.new(school_class_params)
+    @school_class = SchoolClass.new(params.require(:school_class))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
-  def show
-    @school_class = SchoolClass.find_by_id(params[:id])
-  end
-
-  def edit
-    @school_class = SchoolClass.find_by_id(params[:id])
-  end
-
   def update
-    @school_class = SchoolClass.find_by_id(params[:id])
-    @school_class.update(school_class_params)
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
     redirect_to school_class_path(@school_class)
-  end
-
-  private
-  def school_class_params
-    params.require(:school_class).permit(:title, :room_number)
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,25 @@
 class StudentsController < ActionController::Base
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
 
-  def create
-    @student = Student.new(student_params)
-    @student.save
-    redirect_to students_path(@student)
-  end
-
-  def show
-    @student = Student.find_by_id(params[:id])
-  end
-
   def edit
-    @student = Student.find_by_id(params[:id])
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def update
-    @student = Student.find_by_id(params[:id])
-    @student.update(student_params)
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
     redirect_to student_path(@student)
-  end
-  
-  private
-  def student_params
-    params.require(:student).permit(:first_name, :last_name)
   end
 end
