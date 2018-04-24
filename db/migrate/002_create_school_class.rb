eb001c5075a472730df8773a2dd3928e88129779
Sean Oughton <seanoughton@gmail.com>
2018-04-24 00:13:22 +0000

    Done.

--- db/migrate/002_create_school_class.rb
+++ db/migrate/002_create_school_class.rb
@@ -1,10 +1,14 @@
-class CreateSchoolClass < ActiveRecord::Migration
+class CreateSchoolClasses < ActiveRecord::Migration
   def change
-    create_table :schoolclass do |t|
+    create_table :schoolclasses do |t|
       t.string :title
       t.integer :room_number
 
       t.timestamps null: false
     end
   end
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> e3d82bf5e5fa798150b283a1115ba82c94709b36
