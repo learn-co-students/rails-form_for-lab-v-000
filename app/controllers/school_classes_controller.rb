38df5c0ae0a7be8fe58cdc29111f32e987462803
Sean Oughton <seanoughton@gmail.com>
2018-04-24 17:20:48 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,12 +1,13 @@
 class SchoolClassesController < ApplicationController
 
-  def new
-    @schoolclass = SchoolClass.new
+  def new #sends you to the form to create a new schoolclass
+    @schoolclass = SchoolClass.new #creates an empty instance of the school class in memory so that the form will know what class it is referring to
   end
 
-  def create
+  def create # this is where the form data is sent after the form is filled out in the new route
+    #the params that are sent are used to create the new schoolclass
     @schoolclass = SchoolClass.create(params[:school_class])
-    redirect_to school_class_path(@schoolclass)
+    redirect_to school_class_path(@schoolclass) #where does this redirect? to show?
   end
 
   def show
