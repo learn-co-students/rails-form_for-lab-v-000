e393c4d5fa452dd8b08811a10a1ca3628a0a572f
roitblatari <roitblatari@gmail.com>
2018-04-23 21:08:05 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -22,12 +22,11 @@ class SchoolClassesController < ApplicationController
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:student)
+    @school_class.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
     
     redirect_to school_class_path(@school_class)
   end
-
-
+  
   private
 
 	def school_params(*args)
