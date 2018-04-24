149adfc2f95dc1e982c135a2d52d869518260d9a
Sean Oughton <seanoughton@gmail.com>
2018-04-24 16:28:24 +0000

    Done.

--- app/models/testwo.rb
+++
@@ -1,2 +0,0 @@
-class TestTwo < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
-  create_table "schoolclasses", force: :cascade do |t|
+  create_table "school_classes", force: :cascade do |t|
     t.string   "title"
     t.integer  "room_number"
     t.datetime "created_at",  null: false
@@ -25,13 +25,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "test_twos", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "room_number"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
