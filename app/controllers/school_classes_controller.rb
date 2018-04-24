df18a610d5653a95e52bd60f0cc4eb7196058355
Sean Oughton <seanoughton@gmail.com>
2018-04-24 17:09:17 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,11 +1,12 @@
 class SchoolClassesController < ApplicationController
 
   def new
+    binding.pry
   end
 
   def create
-    @school = SchoolClass.create(params[:school_class])
-    redirect_to school_class_path(@school)
+    @school_class = SchoolClass.create(params[:school_class])
+    redirect_to school_class_path(@school_class)
   end
 
   def show
