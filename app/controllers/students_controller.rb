1ce140956913fa83d3b6ee71abdbbc50e49a855b
Eden <emesfun@outlook.com>
2018-04-30 14:06:45 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,15 +17,21 @@ class StudentsController < ApplicationController
     end
 
     def show 
-        @student = Student.find_by(params["id"])
+        @student = Student.find(params["id"])
     end
 
     def edit 
-        @student = Student.find_by(params["id"])
+        @student = Student.find(params["id"])
     end
 
     def update
-        @student = Student.find_by(params["id"])
+        @student = Student.find(params["id"])
+        @student.first_name = params["student"]["first_name"]
+        @student.last_name = params["student"]["last_name"]
+        @student.save
+
+        redirect_to student_path(@student)
+
     end
 
 end
