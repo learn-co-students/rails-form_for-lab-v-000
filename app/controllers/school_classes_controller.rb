27da562420952fc3f252f41a74898d3bd530a5f6
Kim Gonzales <Kimgo589@gmail.com>
2018-05-16 12:32:27 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,37 @@
 class SchoolClassesController < ApplicationController
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+    def show
+        @school_class = get_school_class
+    end
+
+    def edit
+        @school_class = get_school_class
+    end
+
+    def update
+        @school_class = get_school_class
+        @school_class.update(school_class_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+
+    private
+
+    def get_school_class
+        SchoolClass.find(params[:id])
+    end
+
+    def school_class_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,36 @@
-class StudentsController < ApplicationController 
+class StudentsController < ApplicationController
+    
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def show
+        @student = get_student
+    end
+
+    def edit
+        @student = get_student
+    end
+
+    def update
+        @student = get_student
+        @student.update(student_params)
+        redirect_to student_path(@student)
+    end
+
+
+    private
+    def get_student
+        Student.find(params[:id])
+    end
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
 end
