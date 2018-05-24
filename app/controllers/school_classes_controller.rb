c34db13ef70da042ba34151e90163eb391e04390
AlexFriedland <friedlandaj@gmail.com>
2018-05-24 22:02:57 +0200

    controllers logic

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,16 +1,23 @@
 class SchoolClassesController < ApplicationController
   def new
+    @schoolClass = SchoolClass.new
   end
 
   def create
+    @schoolClass = SchoolClass.create(params.require(:school_class))
   end
 
   def show
+    @schoolClass = SchoolClass.find(params[:id])
   end
 
   def edit
+    @schoolClass = SchoolClass.find(params[:id])
   end
 
   def update
+    @schoolClass = SchoolClass.find(params[:id])
+    @schoolClass.update(params.require(:school_class))
+    redirect_to cshool_class_path(@schoolClass)
   end
 end
