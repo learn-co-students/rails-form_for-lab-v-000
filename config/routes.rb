ed4cf86bb92f42d41d0d6b19789606c3bd3183d9
AlexFriedland <friedlandaj@gmail.com>
2018-05-24 21:22:10 +0200

    student and schoolClasses controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'students/new'
+
+  get 'students/create'
+
+  get 'students/show'
+
+  get 'students/edit'
+
+  get 'students/update'
+
+  get 'school_classes/new'
+
+  get 'school_classes/create'
+
+  get 'school_classes/show'
+
+  get 'school_classes/edit'
+
+  get 'school_classes/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
-describe StudentsController do
+RSpec.describe StudentsController, type: :controller do
 
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new student" do
-        expect{
-          post :create, { :student => { :first_name => "Bill", :last_name => "Smith" } }
-        }.to change(Student,:count).by(1)
-      end
-      
-      it "redirects to the new student" do
-        post :create, { :student => { :first_name => "Bill", :last_name => "Smith" } }
-        expect(response).to redirect_to Student.last
-      end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
     end
   end
 
