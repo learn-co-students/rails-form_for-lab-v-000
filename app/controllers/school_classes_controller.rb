478eedb5c2c553ebf456697933dfa2c097689f79
britt <britt.cumens@gmail.com>
2018-06-01 09:16:12 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def new
+    @school_class = SchoolClass.new
+    # this feels wrong.  Why initialize a new instance
+    # if the params are used to create an instance for the db later?
+  end
+
+  def create
+    @school_class = SchoolClass.create(post_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(post_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+end
