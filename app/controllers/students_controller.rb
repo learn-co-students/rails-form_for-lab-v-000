726d04ea806c045734f95082737e63542db8cd0c
ElvinGarcia <dimix181@gmail.com>
2018-06-01 00:04:38 -0400

    features/student_spec unit test passing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,45 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(params[:student])
-    redirect_to (@student)
+    if @student.persisted?
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def show
+    find_user
+  end
+
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+     if @student.update(user_params)
+       redirect_to @student
+     else
+       render 'edit'
+     end
+
+  end
+
+
+
+  private
+
+  def find_user
+    @student = Student.find_by(params[:id])
+  end
+
+  def user_params
+    params.require(:student).permit(:first_name,:last_name)
   end
 
 end
