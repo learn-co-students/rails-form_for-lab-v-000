5827f273977669a0f24ba4865219e04f4a5e002a
katleiahramos <katleiahramos@gmail.com>
2018-06-07 16:41:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,23 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end 
 
+  def new 
+    @student = Student.new
+  end
+
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def edit 
+    @student = Student.find(params[:id])
+  end 
+
+  def update 
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end 
+
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
-  resources :students, only: [:create, :show]
+  resources :students, only: [:create, :show, :new, :edit, :update]
 
 end
