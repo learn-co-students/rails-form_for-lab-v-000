6ef60b40b13addb0263e00fb52ef4c74f114d306
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-16 22:20:26 -0400

    Rspeclemented all CRUD functionality for SchoolClass

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,28 @@
+class SchoolClassesController < ApplicationController
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.create(post_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(post_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+    def post_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+end
