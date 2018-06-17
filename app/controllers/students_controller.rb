124d9d0a644727793ebd07f50f978f24e84b6ecd
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-16 21:45:42 -0400

    Created new form view, fleshed out new controller action, implemented create controller action, implemented show action and view template. 'new form submits content and renders form content' student_spec spec now passing.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,14 @@
 class StudentsController < ApplicationController
+    def show
+        @student = Student.find(params[:id])
+    end
+
     def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
     end
 end
