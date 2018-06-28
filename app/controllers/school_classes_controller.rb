570dad23501f98ac8d3c64aefbb7e49ebca560f2
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 01:05:43 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,6 @@
 class SchoolClassesController < ApplicationController
     def create
-
+        @school_class = SchoolClass.new(params.require(:school_class))
+        @school_class.save
     end
 end
