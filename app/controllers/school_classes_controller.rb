94088ceff191bafe75ac3a1fc902c8c18c6a85d0
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 02:47:58 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,26 @@
 class SchoolClassesController < ApplicationController
-    def create
-        @school_class = SchoolClass.new(params.require(:school_class))
-        @school_class.save
+    def show
+        @school_class = SchoolClass.find(params[:id])
     end
+
+    def create
+        @school_class = SchoolClass.new(params.require(:school_class).permit(:school_class))
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    #def update
+    #    @school_class = SchoolClass.find(params[:id])
+    #    @school_class.update(params.require(:school_class).permit(:school_class))
+    #    redirect_to school_class_path(@school_class)
+    #end
+
+    #def edit
+    #    @school_class = SchoolClass.find(params[:id])
+    #end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,7 @@
 class StudentsController < ApplicationController
-
+    def create
+        @student = Student.new(params.require(:student))
+        @student.save
+        redirect_to student_path(@student)
+    end
 end
