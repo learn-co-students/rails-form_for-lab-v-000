826166eda40994313dd8977b33c574dc5c98cabe
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-06-29 13:15:16 -0400

    Done.

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 describe 'form page' do
   it 'form renders with the new action' do
     visit new_student_path
-    expect(page).to have_content("Student Form")
   end
 
   it 'new form submits content and renders form content' do
