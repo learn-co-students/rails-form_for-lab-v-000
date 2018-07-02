8b8ebe03f642f16e252af92b1ed9289c5f056135
Vanessa Toro <vanetoro84@gmail.com>
2018-07-02 19:12:18 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   resources :students, only: [:index, :new, :create, :show, :edit, :update]
   resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
+
+  # post 'school_classes/new', to: 'school_classes#create',  as: "new"
+  # post 'students/new', to: 'students#create',  as: "new"
 end
