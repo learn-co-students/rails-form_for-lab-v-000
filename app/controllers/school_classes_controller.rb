ed4e9a241d69da8c4065099676d4174c8a1a5a62
Zac Willmington <zac.willmington@gmail.com>
2018-07-06 23:07:06 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,28 @@
+require "pry"
+class SchoolClassesController < ApplicationController
+
+    def index
+        binding.pry
+    end
+
+    def new
+        binding.pry
+    end
+
+    def create
+        binding.pry
+    end
+
+    def show
+        binding.pry
+    end
+
+    def edit
+        binding.pry
+    end
+
+    def update
+        binding.pry
+    end
+
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,26 @@
+require "pry"
+class StudentsController < ApplicationController
+    def index
+        binding.pry
+    end
+
+    def new
+        binding.pry
+    end
+
+    def create
+        binding.pry
+    end
+
+    def show
+        binding.pry
+    end
+
+    def edit
+        binding.pry
+    end
+
+    def update
+        binding.pry
+    end
+end
