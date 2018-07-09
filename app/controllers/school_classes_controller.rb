5e49dbc30223e61e0c0db10e57ff277dc93ff6de
Karl Amoguis <ktamoguis@gmail.com>
2018-07-09 03:48:25 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,20 @@
 class SchoolClassesController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def create
+    binding.pry
+    @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
 end
