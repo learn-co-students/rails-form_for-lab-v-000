a659301de76003c8cefc31eb14011826b2572102
Lucy Wall <lhwall@gmail.com>
2018-07-09 16:50:08 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,29 @@
 class StudentsController < ApplicationController
 
-  def new 
+  def new
+    @student = Student.new
   end
 
-  def create 
-    @student = Student.new(params)
-  end 
+  def create
+    @student = Student.create({:first_name => params[:student][:first_name], :last_name => params[:student][:last_name]})
+    @student.save
+    #byebug
+    redirect_to student_path(@student)
+  end
 
   def show
-  end 
+    @student = Student.find(params[:id])
+  end
 
-  def edit 
-  end 
+  def edit
+    @student = Student.find(params[:id])
+  end
 
   def update
-  end 
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
 
-end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :schools
+  resources :school_classes
   resources :students
 end
