1a2d688e531172b8f08c5b133c768e2822d21879
Michael Pope <popemray1986@gmail.com>
2018-07-14 22:56:28 +0000

    Done.

--- spec/controllers/school_classes_controller_spec.rb
+++ spec/controllers/school_classes_controller_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
-describe SchoolClassesController do
+RSpec.describe SchoolClassesController, type: :controller do
 
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new school_class" do
-        expect{
-          post :create, { :school_class => { :title => "Computer Science", :room_number => 5 } }
-        }.to change(SchoolClass,:count).by(1)
-      end
-      
-      it "redirects to the new school_class" do
-        post :create, { :school_class => { :title => "Computer Science", :room_number => 5 } }
-        expect(response).to redirect_to SchoolClass.last
-      end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
     end
   end
 
