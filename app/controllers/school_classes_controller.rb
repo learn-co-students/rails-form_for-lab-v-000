421a998144c2e48dd627a0362352475cc42e3b4e
epic0448 <samuelogundiran@Yahoo.com>
2018-07-28 17:32:45 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,11 +1,22 @@
 class SchoolClassesController < ApplicationController
+  
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.new 
+    @school_class.title = params[:title]
+	  @school_class.room_number = params[:room_number]
+    @school_class.save
+    redirect_to school_class_path(@school_class.id)  
+     # redirects to the new student A.K.A the new method
+
   end
 
   def show
+    @school_class = SchoolClass.find(params[:id])
+
   end
 
   def edit
@@ -13,4 +24,5 @@ class SchoolClassesController < ApplicationController
 
   def update
   end
-end
+
+end #end of the class method.
