f066b4135d26200d0c23b21d0c975a26db37184e
sgibson47 <sgibson47@gmail.com>
2018-08-14 14:08:29 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(post_params(:title, :room_number))
     @school_class.save
-    redirect_to student_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -20,12 +20,12 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(post_params(:title, :room_number))
-    redirect_to student_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   private
  
   def post_params(*args)
-    params.require(:school_classes).permit(*args)
+    params.require(:school_class).permit(*args)
   end
 end
