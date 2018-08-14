1e9d02971543f6c1d0ed6b44c5645a0760e7b886
sgibson47 <sgibson47@gmail.com>
2018-08-14 13:53:13 -0500

    copy students controller actions into SchoolClassesController

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,27 @@
 class SchoolClassesController < ApplicationController
-  
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(post_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
 
   private
  
