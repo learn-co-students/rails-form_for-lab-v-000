510da89fa27c91b8707bdef87e37c5014680bf8a
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-15 20:51:40 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,40 @@
 
 class StudentsController < ApplicationController
+  before_action :find_student, only: [:show, :update, :edit]
   
   def index
     @students = Student.all
   end
   
   def show
-    @student = Student.find()
   end
   
+  def new
+    @student = Student.new
+  end
   
+  def create
+    @student = Student.new(student_params())
+    @student.save
+    redirect_to student_path(@student)
+  end
+  
+  def update
+    @student.update(student_params())
+    redirect_to student_path(@student)
+  end
+  
+  def edit
+  end
+  
+  private
+  
+    def find_student
+      @student = Student.find(params[:id])
+    end
+  
+    def student_params(*args)
+      params.require(:student).permit(*args)
+    end
+    
 end
