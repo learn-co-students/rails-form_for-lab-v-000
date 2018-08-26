a05b62f8a0c23be48f5eb248938c7bf7251deddc
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-26 20:13:19 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+		@school_classes = SchoolClass.all
+	end
+
+	def show
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
+	def create
+	  @school_class = SchoolClass.new
+	  @school_class.title = params[:title]
+	  @school_class.room_number = params[:room_number]
+	  @school_class.save
+	  redirect_to post_path(@school_class)
+	end
+
+	def edit
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def update
+	  @school_class = SchoolClass.find(params[:id])
+		@school_class.update(params.require(:school_class))
+	  redirect_to post_path(@school_class)
+	end
+end
