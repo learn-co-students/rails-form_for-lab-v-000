9ea37e5b5eb4873b5062e90ab7e8c2c1122839f4
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-26 20:39:18 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,5 @@
 class SchoolClassesController < ApplicationController
 
-  def index
-		@school_classes = SchoolClass.all
-	end
-
 	def show
 		@school_class = SchoolClass.find(params[:id])
 	end
@@ -12,17 +8,15 @@ class SchoolClassesController < ApplicationController
 		@school_class = SchoolClass.new
 	end
 
-	def create
-	  @school_class = SchoolClass.new
-	  @school_class.title = params[:title]
-	  @school_class.room_number = params[:room_number]
-	  @school_class.save
-	  redirect_to post_path(@school_class)
-	end
-
-	def edit
+  def edit
 		@school_class = SchoolClass.find(params[:id])
 	end
+
+  def create
+    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
 
 	def update
 	  @school_class = SchoolClass.find(params[:id])
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,4 @@
-class SchoolClassesController < ApplicationController
-
-  def index
-		@students = Student.all
-	end
+class StudentsController < ApplicationController
 
 	def show
 		@student = Student.find(params[:id])
@@ -12,17 +8,15 @@ class SchoolClassesController < ApplicationController
 		@student = Student.new
 	end
 
-	def create
-	  @student = Student.new
-	  @student.first_name = params[:first_name]
-	  @student.last_name = params[:last_name]
-	  @student.save
-	  redirect_to post_path(@student)
-	end
+  	def edit
+  		@student = Student.find(params[:id])
+  	end
 
-	def edit
-		@student = Student.find(params[:id])
-	end
+  def create
+   @student = Student.new(params.require(:student))
+   @student.save
+   redirect_to student_path(@student)
+ end
 
 	def update
 	  @student = Student.find(params[:id])
