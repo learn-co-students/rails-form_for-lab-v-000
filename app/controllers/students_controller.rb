4f0dc16074ec0667d5d43e34683fbed7f6cb0a27
Jonathan Hong <jonhong777@gmail.com>
2018-09-04 20:29:23 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,18 +4,21 @@ class StudentsController < ApplicationController
   end
 
   def create
-
+    @student = Student.create(first_name: params[:student_first_name], last_name: params[:student_last_name])
+    redirect_to student_path(@student)
   end
 
   def show
-
+    @student = Student.find_by_id(params[:id])
   end
 
   def edit
-
+    @student = Student.find_by_id(params[:id])
   end
 
   def update
-
+    @student = Student.find_by_id(params[:id])
+    @student.update(params[:student])
+    redirect_to student_path(@student)
   end
 end
