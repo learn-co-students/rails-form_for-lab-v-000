a0be01b3b14be2406b6cd0ad27823b916d842d85
Obafemi Fadairo <ofpupp@princeton.edu>
2018-09-06 19:48:32 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class SchoolClassesController < ApplicationController
+
+  def index
+    @school_class = SchoolClass.all
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
+end
