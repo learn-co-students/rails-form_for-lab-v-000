96a831cdf04a32b3041345a4b0ae05d07dd276da
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-13 00:53:47 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -18,8 +18,15 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-    @school_class = SchoolClass.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
+
+  private
+
+def school_class_params(*args)
+	params.require(:school_class).permit(*args)
+end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,7 +17,12 @@ class StudentsController < ApplicationController
   end
 
   def update
-    @student = Student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name,:last_name))
     redirect_to student_path(@student)
+  end
+
+  def student_params(*args)
+  	params.require(:student).permit(*args)
   end
 end
