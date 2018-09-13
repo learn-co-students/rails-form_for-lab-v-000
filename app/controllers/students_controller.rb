f31fe22ec0f9aa80ccc386a080a4dc1416af113d
asabogal <asabogal.git@gmail.com>
2018-09-13 13:04:42 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,41 @@
+class StudentsController < ApplicationController
+
+  #get methods
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  #post/patch methods
+
+  def create
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+
+end
