b8b2417dbc65d43a400159012826bb5bcf973605
mohibhashimi786 <mohib.hashimi@gmail.com>
2018-09-18 22:24:59 -0700

    done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20180919032333) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
@@ -20,4 +20,10 @@ ActiveRecord::Schema.define(version: 1) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "school_classes", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "room_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 end
