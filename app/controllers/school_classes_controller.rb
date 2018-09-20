c9d485cd3abac6e015475affb83f842526e64885
Anu Khambete <anukhambete@gmail.com>
2018-09-20 11:14:11 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class SchoolClassesController < ApplicationController
 
-
+  def create
+    #binding.pry
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,29 @@
+require 'pry'
 class StudentsController < ApplicationController
 
   def create
-
+    #binding.pry
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
+  def show
+    #binding.pry
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @student = Student.find(params[:id])
+  end
 
 end
