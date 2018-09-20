86b846a7f625e0903e8c6454c7f55e10c805f723
Savannah Younts <savyounts@gmail.com>
2018-09-20 17:01:14 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student= Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    set_student
+  end
+
+  def update
+    set_student
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def show
+    set_student
+  end
+
+  private
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    def student_params(*args)
+      params.require(:student).permit(*args)
+    end
+
+end
