0293c4c74a3c01d9a50556f0f3f4acd9717530bd
github username <github email address>
2018-09-26 16:29:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :students, only: [:index, :new, :create, :show, :edit, :update]
+
+     resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
 
   # Example resource route with options:
   #   resources :products do
