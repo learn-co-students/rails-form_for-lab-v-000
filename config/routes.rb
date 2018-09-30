1d53b18e0ca2a4e9669aca09e8f6e0bde1e69937
Nick-Damico <nickalan82@icloud.com>
2018-09-29 22:24:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :students, only: [:index, :new, :create, :show, :edit, :update]
+
+  # Manual examples of routes
+  # get '/school_classes', to: 'school_classes#index'
+  # get '/school_classes/new', to: 'school_classes#new', as: 'new_school_class'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#School_classes
+# SchoolClass.create!(title: "Computer Science", room_number: 115)
+# SchoolClass.create!(title: "Biology", room_number: 109)
+# SchoolClass.create!(title: "Mathmatics", room_number: 102)
+
+
+Student.create!(first_name: "Steph", last_name: 'Haramut')
+Student.create!(first_name: "Kris", last_name: 'Osborne')
+Student.create!(first_name: "Louann", last_name: 'D\'Amico')
