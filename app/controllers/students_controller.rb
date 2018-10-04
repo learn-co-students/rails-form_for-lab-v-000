45ac844ac5cd5efcef7910fdfa607d3cd2aaa127
Kayla Cross <kac288@georgetown.edu>
2018-10-04 15:52:19 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+class StudentsController < ApplicationController
+
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(strong_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(strong_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+    def strong_params(*args)
+      params.require(:student).permit(*args)
+    end
+
+end
