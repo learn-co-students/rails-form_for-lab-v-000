023361eb64a1cd9d54e0a3000fa6f9811b31968b
emanuelbierman <emanuel.bierman@gmail.com>
2018-10-11 12:06:46 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,21 @@
 class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
