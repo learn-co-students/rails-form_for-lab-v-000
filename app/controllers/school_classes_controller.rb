c464bf4d75a0b38b46ad10c9023c19427c4cbaca
Dani Lyn <dlyn1110@gmail.com>
2018-10-18 21:21:16 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,35 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+       @class = SchoolClass.new
+   end
+
+   def create
+       @class = SchoolClass.new(post_params)
+       @class.save
+
+       redirect_to school_class_path(@class)
+   end
+
+   def show
+       @classes = SchoolClass.find(params[:id])
+   end
+
+   def edit
+       @class = SchoolClass.find(params[:id])
+   end
+
+   def update
+       @class = SchoolClass.find(params[:id])
+       @class.update(post_params)
+
+       redirect_to school_class_path(@class)
+   end
+
+   private
+
+   def post_params
+       params.require(:school_class).permit(:title, :room_number)
+   end
+
+ end
