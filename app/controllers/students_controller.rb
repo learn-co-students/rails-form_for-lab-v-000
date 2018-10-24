117edbb8a32bbc7271b1d48812d83fb12bc7b6f9
Daniel <bluh@outlook.com>
2018-10-24 05:00:20 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,15 +4,32 @@ class StudentsController < ApplicationController
   end
 
   def create
-
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def new
     @student = Student.new
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
   def student_params(*args)
 	  params.require(:student).permit(*args)
   end
+
 
 end
