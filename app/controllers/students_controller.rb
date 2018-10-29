d3db0488ac8293c1cdec5e63dbc28ebb6e0899a6
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-10-29 22:32:19 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,15 +12,15 @@ class StudentController < ApplicationController
 	end
 
 	def create
-	  @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+	  @student = Student.new(params.require(:student))
 	  @student.save
-	  redirect_to post_path(@student)
+	  redirect_to student_path(@student)
 	end
 
 	def update
-	  #@post = Post.find(params[:id])
-	  #@post.update(params.require(:post).permit(:title))
-	  #redirect_to post_path(@post)
+	  @student = Student.find(params[:id])
+	  @student.update(params.require(:student))
+	  redirect_to student_path(@post)
 	end
 
 	def edit
