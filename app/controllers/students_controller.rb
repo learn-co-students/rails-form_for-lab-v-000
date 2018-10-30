50e14e0ff8933127d8df6ae515fea318cf51d9f6
rina71 <yurinas@gmail.com>
2018-10-29 23:42:09 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def create
+    @student = Student.new(post_params(:first_name,:last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find_by_id(params[:id])
+    @student.update(post_params(:first_name,:last_name))
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  private
+  def post_params(*args)
+    params.require(:student).permit(*args)
+  end
+end
