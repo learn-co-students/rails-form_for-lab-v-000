2b1cc7674186b3295eb9b5be981782daeb777f76
github username <github email address>
2018-11-06 10:52:12 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101203808) do
+ActiveRecord::Schema.define(version: 2) do
 
-  create_table "schoolclasses", force: :cascade do |t|
+  create_table "school_classes", force: :cascade do |t|
     t.string   "title"
     t.integer  "room_number"
     t.datetime "created_at",  null: false
