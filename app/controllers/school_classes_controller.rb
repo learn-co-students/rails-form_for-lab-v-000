fc6352fdd622ddfe86f1aefdcd035e57cfe57ce1
github username <github email address>
2018-11-12 19:11:19 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,4 +1,10 @@
 class SchoolClassesController < ApplicationController
   def index
+    @classes = SchoolClasses.all
   end
+
+  def show
+    @school_class = SchoolClasses.find(params[:id])
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,10 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
