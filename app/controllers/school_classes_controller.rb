5bca7bd639ab066c001773e0e4edcf0b7814dc35
Rebecca <rtong@buffalo.edu>
2018-11-15 23:37:00 -0500

    10failuresleft

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,15 @@
 class SchoolClassesController < ApplicationController
 
+  def index
+    @schoolclasses = SchoolClass.all
+  end
+
   def new
   end
 
   def create
+    @schoolclass = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    redirect_to "/school_classes/#{@schoolclass.id}"
   end
 
   def show
