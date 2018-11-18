397bb535b99a9810fb89e7eb7883649dee31731a
Brian Birdwell <bbirdwell@gmail.com>
2018-11-18 21:53:52 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,7 @@
 class SchoolClassesController < ApplicationController
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
@@ -10,13 +11,15 @@ class SchoolClassesController < ApplicationController
   end
 
   def show
+    @school_class = SchoolClass.find_by(id: params[:id])
   end
 
   def edit
+      @school_class = SchoolClass.find_by(id: params[:id])
   end
 
   def update
-    @school_class = Post.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
 
     def new
+      @student = Student.new
     end
 
     def create
@@ -10,13 +11,15 @@ class StudentsController < ApplicationController
     end
 
     def show
+      @student = Student.find_by(id: params[:id])
     end
 
     def edit
+      @student = Student.find_by(id: params[:id])
     end
 
     def update
-      @student = Post.find(params[:id])
+      @student = Student.find(params[:id])
       @student.update(student_params(:first_name, :last_name))
       redirect_to student_path(@student)
     end
