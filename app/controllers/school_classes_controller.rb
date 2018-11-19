e0779faa9b8e6dc4d2472e5f33dad43e8a57c94c
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-19 16:25:19 -0700

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,17 @@
+class SchoolClassesController < ApplicationController
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+end
