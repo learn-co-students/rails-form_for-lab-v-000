83b0eee6f75de982ec46686a800862c49c3e0dcc
SMartinSci <sm674@cornell.edu>
2018-11-26 05:49:11 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,7 +8,7 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def create
-	  @school_class = SchoolClass.new(params.require(:school_class))
+	  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
 		@school_class.save
 		redirect_to school_class_path(@school_class)
 	end
@@ -18,8 +18,13 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def update
-		@school_class = school_class.find(params[:id])
-		@school_class.update(params.require(:school_class))
-		redirect_to school_class_path(@school_class)
+	 	@school_class = SchoolClass.find(params[:id])
+		@school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
 	end
+	
+	def school_class_params(*args)
+		params.require(:school_class).permit(*args)
+	end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,19 +7,23 @@ class StudentsController < ApplicationController
 		@student = Student.new
 	end
 
-	def edit
-	  @student = Student.find(params[:id])
-	end
-
 	def create
-	    @student = Student.new(params.require(:student))
+	    @student = Student.new(student_params(:first_name, :last_name))
 		@student.save
 		redirect_to student_path(@student)
 	end
 
+	def edit
+	  @student = Student.find(params[:id])
+	end
+
 	def update
-		@student = student.find(params[:id])
-		@student.update(params.require(:student))
-		redirect_to student_path(@student)
+	  	@student = Student.find(params[:id])
+		@student.update(student_params(:first_name, :last_name))
+    	redirect_to student_path(@student)
+	end
+
+	def student_params(*args)
+		params.require(:student).permit(*args)
 	end
 end
