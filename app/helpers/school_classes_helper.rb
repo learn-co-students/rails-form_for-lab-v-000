5612d6d7441da97e427e35b6c3a542796fbe4e77
Judy McGoldrick <jjeanius@verizon.net>
2019-01-03 22:44:30 +0000

    Done.

--- app/helpers/school_classes_helper.rb
+++ app/helpers/school_classes_helper.rb
@@ -1,3 +1,8 @@
+require 'pry'
 module SchoolClassesHelper
+
+  def concat_strings(string1, string2)
+     string1 + " " + string2
+  end
 
 end
--- spec/helpers/school_classes_helper_spec.rb
+++ spec/helpers/school_classes_helper_spec.rb
@@ -11,5 +11,9 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SchoolClassesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+     describe "string concat" do
+       it "concats two strings with spaces" do
+         expect(helper.concat_strings("this","that")).to eq("this that")
+       end
+     end
+   end
