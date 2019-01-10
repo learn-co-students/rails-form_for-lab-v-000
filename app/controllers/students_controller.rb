a71c54e30420aeffde1ada40b026e95d6b5e533a
Grace S <vocalise@gmail.com>
2019-01-09 21:44:25 -0800

    Added edit/update views and controller actions for students and school_classes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,8 +16,19 @@ class StudentsController < ApplicationController
     end
 
     def create
-        @student = Student.new(student_params)
+        @student = Student.new(student_params(:first_name, :last_name))
         @student.save
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params(:first_name, :last_name))
+
         redirect_to student_path(@student)
     end
 
