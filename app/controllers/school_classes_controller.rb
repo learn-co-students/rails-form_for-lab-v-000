92e131357a8caa919d72297ff4b7d558a3442153
Harley Rayner <harleymteden@gmail.com>
2019-01-15 20:23:05 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,22 +3,37 @@ require 'pry'
 class SchoolClassesController < ApplicationController
 
   def index
-    binding.pry
+    # binding.pry
   end
 
   def show
+    # binding.pry
+    @school_class = SchoolClass.find_by_id(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
   end
 
   def create
-	  @school_class = SchoolClass.create(title: params[:title], room_number: params[:room_number])
-	  redirect_to school_class_path(@school_class)
+	  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
 	end
+
+
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params(:title))
+	  redirect_to post_path(@post)
+	end
+
 
   private
 
-  # def post_params(*args)
-  #   params.require(:post).permit(*args)
-  # end
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :students, only: %i[index show new create]
+  resources :students, only: %i[index show new create update]
 
-  resources :school_classes, only: %i[index show new create]
+  resources :school_classes, only: %i[index show new create update]
   #
   # get 'schoolclass/:id/edit', to: 'articles#edit', as: :edit_article
   #
