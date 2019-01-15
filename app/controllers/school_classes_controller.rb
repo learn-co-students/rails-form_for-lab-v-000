c5dc63ddabc287e9f8545c73f1254236688362db
Barbara Choy <barbchoy@gmail.com>
2019-01-15 21:25:48 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,27 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+end
