1bed43674e3ba5516cb0c6514141123b0f5abd6e
Zachary Lehmann <zlehmann@gmail.com>
2019-01-16 21:26:11 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,18 @@
 class SchoolClassesController < ApplicationController
+  def create
+    @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    redirect_to school_class_path(@school_class)
+  end
 
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,19 @@
 class StudentsController < ApplicationController
-  post '/create' do
+  def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to '/student/#{@student.id}'
+    redirect_to student_path(@student)
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
   end
 end
