fdd808c79417543f544b52d04c05d7a1d5b14224
Zachary Lehmann <zlehmann@gmail.com>
2019-01-16 21:43:54 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,11 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,6 @@ ActiveRecord::Schema.define(version: 20190116203235) do
     t.integer "room_number"
   end
 
-  create_table "schoolclasses", force: :cascade do |t|
-    t.string  "title"
-    t.integer "room_number"
-  end
-
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
