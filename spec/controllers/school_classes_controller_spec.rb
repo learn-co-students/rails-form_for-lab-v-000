d0d4a247f5e19ba6cf4cf2fcde43de34004096ab
github username <github email address>
2019-01-21 11:53:00 -0600

    Done.

--- spec/controllers/school_classes_controller_spec.rb
+++ spec/controllers/school_classes_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe SchoolClassesController do
-
   describe "POST create" do
     context "with valid attributes" do
       it "creates a new school_class" do
@@ -9,7 +8,7 @@ describe SchoolClassesController do
           post :create, { :school_class => { :title => "Computer Science", :room_number => 5 } }
         }.to change(SchoolClass,:count).by(1)
       end
-      
+
       it "redirects to the new school_class" do
         post :create, { :school_class => { :title => "Computer Science", :room_number => 5 } }
         expect(response).to redirect_to SchoolClass.last
