d6b059ed693f716dd6d585f88c7bc7692b2cb7af
George  Pianka <gpianka1@gmail.com>
2019-01-28 09:52:10 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.create(title: "Computer Science", room_number: 5)
+Student.create(first_name: "Daenerys", last_name: "Targaryen")
