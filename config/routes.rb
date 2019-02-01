d0ef41f00fb1ec6d884bb0c3380b6091e5ee4536
nmarini <nmarini44@gmail.com>
2019-02-01 14:04:33 -0800

    added controller school classes and views for new school classes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show, :new, :create, :edit, :update]
-  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, only: [:show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:show, :new, :create, :edit, :update]
 end
