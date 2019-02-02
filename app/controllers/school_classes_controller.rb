ab72bacc9d1d242663a3733279cc605c36b53fe0
nmarini <nmarini44@gmail.com>
2019-02-01 16:20:09 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -20,7 +20,7 @@ class SchoolClassesController < ApplicationController
 
     def create 
         @school_class = SchoolClass.create(params.require(:school_class))
-        redirect_to school_class_path(@school_class
+        redirect_to school_class_path(@school_class)
 
     end 
 
