bd174d88fdaafdd8edb01020e80f423dfcd9e85e
EdselCaprice <caprice@mail.usf.edu>
2019-02-01 20:50:39 -0800

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,22 @@
+class SchoolClasses < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def show
+    end
+
+    def edit
+    end
+    
+    def update
+    end
+    
+
+end
