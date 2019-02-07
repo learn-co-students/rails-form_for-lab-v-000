81eb7402e946bc0135c802dec0aacf6ef148cca6
Erin Green <erin.y.green@gmail.com>
2019-02-07 16:35:45 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,28 @@
 class StudentsController < ApplciationController
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.title = params[:title]
+    @student.description = params[:description]
+    @student.save
+    redirect_to Student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(title: params[:Student][:title], description: params[:Student][:description])
+    redirect_to Student_path(@student)
+  end
 end
