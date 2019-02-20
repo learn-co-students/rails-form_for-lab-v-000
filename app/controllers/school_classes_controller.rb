5852cb9ae789b1d3ba5cc29aeb23b822a14115ae
catherinism <catcreates13@gmail.com>
2019-02-19 21:48:11 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,14 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show
+   @school_class = SchoolClass.find(params[:id])
+ end
+
+ def new
+ end
+
+end
