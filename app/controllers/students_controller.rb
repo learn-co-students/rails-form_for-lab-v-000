676708e457945b729d27fbc9254f6ce465ad8746
catherinism <catcreates13@gmail.com>
2019-02-20 21:11:19 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,24 @@ class StudentsController < ApplicationController
  end
 
  def new
+   @student = Student.new
  end
+
+ def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
+  end
+
 
 end
