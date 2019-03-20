c19be294207561f5d9520c8620384fd0327d3fa8
Jonathan Luk <jluk87@gmail.com>
2019-03-20 21:58:31 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def edit
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def update
+    binding.pry
+    @student = Student.find_by_id(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+end
--- spec/controllers/school_classes_controller_spec.rb
+++ spec/controllers/school_classes_controller_spec.rb
@@ -1,20 +1,46 @@
 require 'rails_helper'
 
+RSpec.describe SchoolClassesController, type: :controller do
 
-describe SchoolClassesController do
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new school_class" do
-        expect{
-          post :create, params: { :school_class => { :title => "Computer Science", :room_number => 5 } }
-        }.to change(SchoolClass,:count).by(1)
-      end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      it "redirects to the new school_class" do
-        post :create, params: { :school_class => { :title => "Computer Science", :room_number => 5 } }
-        expect(response).to redirect_to SchoolClass.last
-      end
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
     end
   end
 
