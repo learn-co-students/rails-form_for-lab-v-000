37060c550a75214ff1ee14981645f5bdb0393f8a
Ashley Wong <ashwong04@gmail.com>
2019-04-01 18:01:20 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,9 +18,12 @@ class StudentsController < ApplicationController
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    redirect_to student_path(@student)
   end
 
 end
