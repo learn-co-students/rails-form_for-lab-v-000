0219b139943d45b793d82d43a303beaad88c63bf
Ashley Wong <ashwong04@gmail.com>
2019-04-01 17:25:26 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -18,7 +18,14 @@ class SchoolClassesController < ApplicationController
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
 
 end
