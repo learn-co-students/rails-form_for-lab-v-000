f785b7f7eeb9d5213c4117ac5effce7676b902a5
jaywood128 <jaywood128@gmail.com>
2019-03-31 18:13:20 -0700

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,14 +1,15 @@
 class SchoolClassesController < ApplicationController 
   def new 
+    @school_class = SchoolClass.new 
   end
 
   def create 
     @school_class = SchoolClass.create(post_params)
-    @school_class.save 
     redirect_to school_class_path(@school_class)
   end
 
   def show 
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def edit 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :students, only: [:new, :create, :show, :update]
 
-  post 'schoolclass/new', to: 'schoolclass#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
