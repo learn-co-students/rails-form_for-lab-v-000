89b1393d175e3d397961cf2b97286dbefe07e1d7
Ashley Wong <ashwong04@gmail.com>
2019-04-01 16:08:43 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,6 +8,17 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+  end
+
 
 end
