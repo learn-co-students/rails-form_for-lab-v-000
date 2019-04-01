0acfc12efe4ecb3d0156f74142a73b80441ac465
Ashley Wong <ashwong04@gmail.com>
2019-04-01 17:46:47 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,26 @@
 class StudentsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
