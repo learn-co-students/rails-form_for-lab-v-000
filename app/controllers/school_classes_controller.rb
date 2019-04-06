6ab06eb56c256fc258103727d5ed4c166a8cba70
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-06 17:22:52 -0400

    Create controllers

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,25 @@
+class SchoolClassesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
