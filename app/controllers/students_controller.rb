a762930283519088851592ba3c1e61dce77b90d5
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-06 17:37:21 -0400

    Add logic to controller actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,33 @@
 class StudentsController < ApplicationController
   def index
-
+    @students = Student.all
   end
 
   def new
-
+    @student = Student.new
   end
 
   def create
-
+    @student = Student.create(form_params)
+    redirect_to student_path(@student)
   end
 
   def show
-
+    @student = Student.find(params[:id])
   end
 
   def edit
-
+    @student = Student.find(params[:id])
   end
 
   def update
-
+    @student = Student.find(params[:id])
+    @student.update(form_params)
+    redirect_to student_path(@student)
   end
+
+  private
+    def form_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
 end
