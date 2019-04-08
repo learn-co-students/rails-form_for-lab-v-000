6779eaa234660617467f127cf9de53ddf8179f13
Christine Cua <cloudycua@gmail.com>
2019-04-08 23:32:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :school_classes
-  resources :students
+  resources :school_classes, only: %i[index new create edit update show]
+  resources :students, only: %i[index new create edit update show]
 end
