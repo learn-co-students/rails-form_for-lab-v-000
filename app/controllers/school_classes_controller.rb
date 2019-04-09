6c0b7ce21c9b1b34fd6374e306183c8d7b160136
Christine Cua <cloudycua@gmail.com>
2019-04-09 02:48:22 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -2,7 +2,7 @@ class SchoolClassesController < ApplicationController
   def index
     @school_classes = SchoolClass.all
   end
-  
+
   def show
     @school_class = SchoolClass.find(params[:id])
   end
