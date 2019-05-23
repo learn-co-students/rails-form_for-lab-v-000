9e8670d46d5c75f5b17d51b7bca79eafd15e3f58
Romeo Gono <romeogono1@aol.com>
2019-05-23 04:04:30 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+  
+  def new 
+    @student = Student.new
+  end
+  
+  def create 
+    @student = Student.new(post_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+  
+  def show 
+    @student = Student.find(params[:id])
+  end 
+  
+  def edit
+    @student = Student.find(params[:id])
+  end 
+  
+  def update 
+    @student = Student.find(params[:id])
+    @student.update(post_params)
+    redirect_to student_path(@student)
+  end 
+  
+  def post_params
+  params.require(:student).permit(:first_name, :last_name)
+  end
+  
+end
