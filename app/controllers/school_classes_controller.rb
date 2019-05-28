94d01396148de50d865ae321123e7bb9b92f4d13
nanton83 <nicholas.romans@gmail.com>
2019-05-28 13:06:19 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,16 @@
 class SchoolClassesController < ApplicationController
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+	    @school_class.save
+    end
+
+    def school_class_params
+        params.require(:school_class).permit!
+      end
+
 end
