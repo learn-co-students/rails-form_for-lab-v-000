5ccf877ae266bb753216ee32575d84911005934a
NathalieSantana <nathalie.scruz@gmail.com>
2019-06-20 23:55:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:index, :create, :show, :edit]
+  resources :schoolclass, only: [:index, :create, :show, :edit]
+  
+
 end
