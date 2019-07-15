179f860da5737dfcc510b5a4c240734f82df320b
Kat Tochowicz <ktochow1@binghamton.edu>
2019-07-15 11:29:21 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(post_params)
+    # binding.pry
+    # @student.first_name = @student.params[:student][:first_name]
+    # @student.last_name = @student.params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def post_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+end
