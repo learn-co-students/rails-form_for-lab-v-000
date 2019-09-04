f90d94e8dc2cea7266587d1082c125a99a795020
david-forer <david.j.forer@gmail.com>
2019-09-04 18:11:19 +0900

    changed the forms so they worked properly

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,37 +1,37 @@
 class SchoolClassesController < ApplicationController
 
-   def index
+  def index
     @school_classes = SchoolClass.all
-   end
+  end
 
-   def new
+  def new
     @school_class = SchoolClass.new
-   end
-   
-   def create
+  end
+
+  def create
     @school_class = SchoolClass.new(post_params(:title, :description))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
-   def show
+  def show
     @school_class = SchoolClass.find(params[:id])
   end
 
-   def edit
+  def edit
     @school_class = SchoolClass.find(params[:id])
   end
 
-   def update
+  def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(post_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
 
-   private
+  private
 
-   def post_params(*args)
+  def post_params(*args)
     params.require(:school_class).permit(*args)
   end
-end   
+end
    
