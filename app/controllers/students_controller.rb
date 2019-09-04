17580e2d97d7936c5bc18ce4332ec9a3fe46bfec
david-forer <david.j.forer@gmail.com>
2019-09-04 17:55:19 +0900

    fixed mistake

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,38 +1,36 @@
-class SudentsController < ApplicationController
+class StudentsController < ApplicationController
 
   def index
     @students = Student.all
-   end
+  end
 
-   def new
+  def new
     @student = Student.new
-   end
-   
-   def create
-    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
+  end
+
+  def create
+    @student = Student.new(post_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
 
-   def show
+  def show
     @student = Student.find(params[:id])
   end
 
-   def edit
+  def edit
     @student = Student.find(params[:id])
   end
 
-   def update
+  def update
     @student = Student.find(params[:id])
-    @student.update(params.require(:student).permit(:first_name, :last_name))
+    @student.update(post_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
-   private
+  private
 
-   def post_params(*args)
+  def post_params(*args)
     params.require(:student).permit(*args)
   end
-   
-   
 end
