382ee393523a84d66e266e563c443eb7d9cca0f5
Rachel Grier <rgrier104@gmail.com>
2019-09-17 18:24:12 +0000

    Done.

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,6 +1,6 @@
-class CreateSchoolclasses < ActiveRecord::Migration
+class CreateSchoolClasses < ActiveRecord::Migration
   def change
-    create_table :schoolclasses do |t|
+    create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
 
