d3d7179addb6e47acb1a52b63c3a4bb8793477e0
sdcro <sdcrouse91@gmail.com>
2019-11-21 18:15:07 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  #resources :students, except: :destroy 
+  # This actually works! I get every route except 'destroy'.
+  # 'resources :students, not: :destroy' is legal code, but it does something different.
+  # It generates all of the routes, and one example controller action is 'students#edit {:not=>:destroy}'
 end
