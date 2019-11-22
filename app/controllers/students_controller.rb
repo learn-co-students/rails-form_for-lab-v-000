fcefbd225adb81e80ab6093672bc8aa066264f7c
sdcro <sdcrouse91@gmail.com>
2019-11-22 01:23:35 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,30 @@
 class StudentsController < ApplicationController
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to student_path(student)
+  end
+
+  def edit
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def update
+    student = Student.find_by_id(params[:id])
+    student.update(student_params)
+    redirect_to student_path(student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
