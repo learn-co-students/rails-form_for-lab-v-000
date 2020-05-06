43a147825d770f908346c2320debdcf956e86e23
Reno <renojames19@gmail.com>
2020-05-05 20:22:57 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,4 +15,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
 end
