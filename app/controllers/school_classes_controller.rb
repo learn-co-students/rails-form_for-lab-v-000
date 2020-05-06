179ff6ce02778360a57c3ff48c2613af6680a0bc
renojames <renojames19@gmail.com>
2020-05-05 19:56:36 -0700

    All tests but update passing
    All tests but update passing

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,18 @@
 class SchoolClassesController < ApplicationController
 
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new
+    @school_class.update(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
 end
