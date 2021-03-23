0a9748d71609784063fe6e9f22e21b4dc84d0c02
parisdolfman <parisdolfman@gmail.com>
2021-03-23 11:03:51 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,9 @@
 class StudentsController < ApplicationController
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@students)
+  end   
     
 end 
